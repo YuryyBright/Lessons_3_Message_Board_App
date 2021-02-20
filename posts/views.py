98f06d7371cfd500c02de01b73759d962fb47bb0
Ostrovskiy Yryy <yura.ostrovskiy.02@gmail.com>
2021-02-20 21:28:13 +0200

    *

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Posts
-
-class HomePageView(ListView):
-    model = Posts
-    template_name = 'lessons_3/home.html'
-
+# from django.shortcuts import render
+# from django.views.generic import ListView
+# from .models import Posts
+#
+# class HomePageView(ListView):
+#     model = Posts
+#     template_name = 'home.html'
+#     context_object_name = 'posts'
+#
